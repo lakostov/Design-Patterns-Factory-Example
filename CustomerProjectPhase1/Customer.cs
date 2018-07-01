602c7a6f1c5689e75c548ea4297a3e688ea590bd
Lachezar Kostov <lachezarkostov@gmail.com>
2018-07-01 21:03:19 +0100

    InitialCommit

---
+++ CustomerProjectPhase1/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleLayer
+{
+    public class CustomerBase
+    {
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Address { get; set; }
+        public virtual void Validate()
+        {
+            throw new Exception("Not Implemented");
+        }
+    }
+
+    public class Customer : CustomerBase
+    {
+        public override void Validate()
+        {
+           if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (BillAmount == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (BillDate > DateTime.Now)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (Address.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+        }
+    }
+
+    public class Lead : CustomerBase
+    {
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+        }
+    }
+}
---
+++ FactoryCustomer/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiddleLayer;
+
+namespace FactoryCustomer
+{
+    public static class Factory //DP Simple Factory pattern
+    {
+        private static Dictionary<string, CustomerBase> custs = new Dictionary<string, CustomerBase>();
+       
+        public static CustomerBase Create(string TypeCustomer)
+        {
+            //DP Lazy loading
+            if (custs.Count == 0)
+            {
+                custs.Add("Customer", new Customer());
+                custs.Add("Lead", new Lead());
+            }
+                //DP RIP pattern Return if with polymorphism
+                return custs[TypeCustomer];
+        }
+    }
+}
